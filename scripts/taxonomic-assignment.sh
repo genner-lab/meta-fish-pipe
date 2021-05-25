24299bd25c0a82fb6140bff64b1dfafed8e00afa
rupert@it031740 <rupertcollins@gmail.com>
2021-05-25 21:45:45 +0100

    new code added

--- scripts/taxonomic-assignment.sh
+++ scripts/taxonomic-assignment.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
+# set params #
+while getopts t:p: option
+do
+case "${option}"
+in
+t) THREADS=${OPTARG};;
+p) PRIMER=${OPTARG};;
+esac
+done
 
 
 
@@ -7,42 +16,102 @@
 ############## MERGE AND DEREP ASVs ##############
 
 # cat all the asvs
-find temp/processing -name 'asvs-clean.fasta' -exec cat {} \; > temp/reference-library/asvs-clean-cat.fasta
+find temp/processing -name 'asvs-clean.fasta' -exec cat {} \; > temp/taxonomic-assignment/asvs-clean-cat.fasta
 
 # add md5sums
-vsearch --fastx_subsample temp/reference-library/asvs-clean-cat.fasta --sample_pct 100 --relabel_md5 --relabel_keep --fasta_width 0 --fastaout temp/reference-library/asvs-clean-cat-relabel.fasta
+vsearch --fastx_subsample temp/taxonomic-assignment/asvs-clean-cat.fasta --sample_pct 100 --relabel_md5 --relabel_keep --fasta_width 0 --fastaout temp/taxonomic-assignment/asvs-clean-cat-relabel.fasta
 
 # make a table of asvs
-echo "md5,primer,lib,asv" > temp/reference-library/asvs-all.csv
-grep ">" temp/reference-library/asvs-clean-cat-relabel.fasta | sed -e 's/>//g' -e 's/ /,/g' -e 's/-/,/g' >> temp/reference-library/asvs-all.csv
+echo "md5,primer,lib,asv" > temp/taxonomic-assignment/asvs-all.csv
+grep ">" temp/taxonomic-assignment/asvs-clean-cat-relabel.fasta | sed -e 's/>//g' -e 's/ /,/g' -e 's/-/,/g' >> temp/taxonomic-assignment/asvs-all.csv
 
 # derep the ASVs
-vsearch --derep_fulllength temp/reference-library/asvs-clean-cat-relabel.fasta --minuniquesize 1 --fasta_width 0 --output temp/reference-library/asvs-clean-cat-relabel-derep.fasta
+vsearch --derep_fulllength temp/taxonomic-assignment/asvs-clean-cat-relabel.fasta --minuniquesize 1 --fasta_width 0 --output temp/taxonomic-assignment/asvs-clean-cat-relabel-derep.fasta
 
 
 ############## RUN SINTAX ON REFSEQ ##############
 ############## RUN SINTAX ON REFSEQ ##############
 
 # run sintax
-vsearch --threads 8 --sintax temp/reference-library/asvs-clean-cat-relabel-derep.fasta --db temp/reference-library/refseq-annotated.fasta --sintax_cutoff 0.7 --tabbedout temp/reference-library/sintax-output.tsv
+vsearch --threads "$THREADS" --sintax temp/taxonomic-assignment/asvs-clean-cat-relabel-derep.fasta --db assets/refseq-annotated.fasta --sintax_cutoff 0.7 --tabbedout temp/taxonomic-assignment/sintax-output.tsv
 
 
+############## EXTRACT FISH ASVs ##############
+############## EXTRACT FISH ASVs ##############
+
+scripts/extract-fishes.R
 
 
-############## MAKE BLAST DB ##############
-############## MAKE BLAST DB ##############
+############## RUN BLAST ON FISH ASVs ##############
+############## RUN BLAST ON FISH ASVs ##############
 
 # make blast db (only need to do this step once)
-makeblastdb -in ../temp/reference-library/custom-refs.fasta -dbtype nucl -blastdb_version 5
+cp assets/custom-reference-library.fasta temp/taxonomic-assignment/custom-reference-library.fasta
+makeblastdb -in temp/taxonomic-assignment/custom-reference-library.fasta -dbtype nucl -blastdb_version 5
 
 # blast the blast db
 # get better hits with smaller word size
-blastn -task blastn -num_threads 8 -evalue 1000 -word_size 7 -max_target_seqs 500 -db ../reference-library/custom-refs.fasta -outfmt "6 qseqid sseqid evalue length pident nident score bitscore" -out "$DIR"/results/fish-blast.out -query "$DIR"/results/fishqueries.fasta
+blastn -task blastn -num_threads "$THREADS" -evalue 1000 -word_size 7 -max_target_seqs 500 -db temp/taxonomic-assignment/custom-reference-library.fasta -outfmt "6 qseqid sseqid evalue length pident nident score bitscore" -out temp/taxonomic-assignment/fish-blast.out -query temp/taxonomic-assignment/asvs-fish.fasta
 
 # join the header
-echo -e "asv\tblastDbid\tblastEvalue\tblastLength\tblastPident\tblastNident\tblastScore\tblastBitscore" > "$DIR"/results/headers
-cat "$DIR"/results/headers "$DIR"/results/fish-blast.out > "$DIR"/results/fish-blast-result.tsv
-rm "$DIR"/results/fish-blast.out
-rm "$DIR"/results/headers
+echo -e 'asv\tblastDbid\tblastEvalue\tblastLength\tblastPident\tblastNident\tblastScore\tblastBitscore' | sed -e "s/-e //g" > temp/taxonomic-assignment/headers
+cat temp/taxonomic-assignment/headers temp/taxonomic-assignment/fish-blast.out > temp/taxonomic-assignment/fish-blast-result.tsv
+rm temp/taxonomic-assignment/fish-blast.out
+rm temp/taxonomic-assignment/headers
 
+
+############## PROCESS BLAST RESULTS ##############
+############## PROCESS BLAST RESULTS ##############
+
+scripts/process-blast.R
+
+
+############## PREP FOR EPA ASSIGNMENT ##############
+############## PREP FOR EPA ASSIGNMENT ##############
+
+# process the alignment for EPA
+mkdir -p temp/taxonomic-assignment/epa
+
+#scripts/prep-epa.R -p tele02
+scripts/prep-epa.R -p "$PRIMER"
+
+
+############## ALIGN AND RUN EPA ##############
+############## ALIGN AND RUN EPA ##############
+
+# align with mafft (accurate)
+mafft --thread "$THREADS" --maxiterate 1000 --genafpair temp/taxonomic-assignment/epa/epa.input.fasta > temp/taxonomic-assignment/epa/epa.aligned.fasta
+
+# split up references and queries for epa
+scripts/split.R
+
+# make RAxML trees for all markers
+# create a binary and set model
+raxml-ng --msa temp/taxonomic-assignment/epa/epa.references.fasta --model TN93+G --parse
+
+# run a tree search
+raxml-ng --msa temp/taxonomic-assignment/epa/epa.references.fasta.raxml.rba --tree pars{1} --search --seed 42 --threads "$THREADS"
+
+# optimise params
+raxml-ng --msa temp/taxonomic-assignment/epa/epa.references.fasta.raxml.rba --evaluate --tree temp/taxonomic-assignment/epa/epa.references.fasta.raxml.rba.raxml.bestTree --prefix temp/taxonomic-assignment/epa/opt
+
+# run epa-ng
+epa-ng --ref-msa temp/taxonomic-assignment/epa/epa.references.fasta --tree temp/taxonomic-assignment/epa/epa.references.fasta.raxml.rba.raxml.bestTree --query temp/taxonomic-assignment/epa/epa.queries.fasta --outdir temp/taxonomic-assignment/epa --model temp/taxonomic-assignment/epa/opt.raxml.bestModel --redo --preserve-rooting off
+
+# run gappa
+gappa examine assign --per-query-results --jplace-path temp/taxonomic-assignment/epa/epa_result.jplace --taxon-file temp/taxonomic-assignment/epa/references.taxonomy.tsv --out-dir temp/taxonomic-assignment/epa
+
+# rename best tree to keep
+mv temp/taxonomic-assignment/epa/epa.references.fasta.raxml.rba.raxml.bestTree temp/taxonomic-assignment/epa/references.tree.nwk
+
+# clean up
+rm temp/taxonomic-assignment/epa/*.log
+rm temp/taxonomic-assignment/epa/*raxml*
+
+# process results
+scripts/process-epa.R
+
+
+############## COMBINE TAXONOMIC ASSIGNMENT RESULTS ##############
+############## COMBINE TAXONOMIC ASSIGNMENT RESULTS ##############
 
