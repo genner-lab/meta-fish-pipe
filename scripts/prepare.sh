a61dd3d2c73e6f79f0e3c9dbfca71cba1fb78569
rupert@it031740 <rupertcollins@gmail.com>
2021-05-27 21:48:43 +0100

    fixed mafft and vsearch

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -20,14 +20,14 @@ Rscript scripts/get-r-libs.R
 # add the installed software
 { printf "\n\n-----------------------\nLinux software versions\n-----------------------\n"; } >> temp/sessionInfo.txt
 { printf "\ncutadapt --version\n" & cutadapt --version; } >> temp/sessionInfo.txt
-{ printf "\ngappa --version\n" & gappa --version; } >> temp/sessionInfo.txt
-{ printf "\nepa-ng --version\n" & epa-ng --version; } >> temp/sessionInfo.txt
-{ printf "\nraxml-ng --version\n" & raxml-ng --version | grep "RAxML-NG"; } >> temp/sessionInfo.txt
-#{ printf "\nmafft --version\n" & mafft --version; } >> temp/sessionInfo.txt
+{ printf "\nvsearch --version\n" & vsearch --version 2>&1 | grep "linux"; } >> temp/sessionInfo.txt
 { printf "\nhmmsearch -h\n" & hmmsearch -h | grep "HMMER"; } >> temp/sessionInfo.txt
 { printf "\nblastn -version\n" & blastn -version | grep -v "Package"; } >> temp/sessionInfo.txt
-#{ printf "\nvsearch --version\n" & vsearch --version | grep "linux"; } >> temp/sessionInfo.txt
-{ printf "\nfqtools -v\n" & fqtools -v; } >> temp/sessionInfo.txt
+{ printf "\nmafft --version\n" & mafft --version 2>&1; } >> temp/sessionInfo.txt
+{ printf "\nraxml-ng --version\n" & raxml-ng --version | grep "RAxML-NG"; } >> temp/sessionInfo.txt
+{ printf "\nepa-ng --version\n" & epa-ng --version; } >> temp/sessionInfo.txt
+{ printf "\ngappa --version\n" & gappa --version; } >> temp/sessionInfo.txt
+{ printf "\nseqkit -v\n" & seqkit version | grep '^seqkit'; } >> temp/sessionInfo.txt
 
 # copy to results
 cp temp/sessionInfo.txt results/sessionInfo.txt
