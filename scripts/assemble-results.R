cdc2d1ab88f49168d849c9287f0b035e70aedb05
rupert@it031740 <rupertcollins@gmail.com>
2021-05-27 16:06:16 +0100

    finished assemble script

--- scripts/assemble-results.R
+++ scripts/assemble-results.R
@@ -1,8 +1,14 @@
 #!/usr/bin/env Rscript
 
+# report
+writeLines("\nAssembling results ...\n")
+
 # load libs and funs
 source(here::here("scripts/funs-libs.R"))
 
+
+############## LOAD DATA AND JOIN ##############
+############## LOAD DATA AND JOIN ##############
 
 # reload global blast, local blast, epa results
 fish.otus <- suppressMessages(suppressWarnings(read_csv(file=here("temp/taxonomic-assignment/sintax-table.csv"))))
@@ -17,7 +23,11 @@ dnas <- read.FASTA(file=here("temp/taxonomic-assignment/asvs-clean-cat-relabel-d
 dnas.table <- tibble(asv=names(dnas),nucleotides=mapply(paste,collapse="",as.character(dnas),USE.NAMES=FALSE)) %>% mutate(asvLength=str_length(nucleotides))
 
 # combine
-taxonomy.results <- purrr::reduce(list(fish.otus,local.db.blast.sorted,epa.results.filtered,dnas.table),left_join, by="asv") %>% rename(asvHash=asv)
+taxonomy.results <- purrr::reduce(list(fish.otus,local.db.blast.sorted,epa.results.filtered,dnas.table),left_join,by="asv") %>% rename(asvHash=asv)
+
+
+############## ASSIGN TAXONOMY ##############
+############## ASSIGN TAXONOMY ##############
 
 # set a 90% of modal length threshold
 minLen <- ceiling(mode_avg(pull(taxonomy.results,asvLength))*0.9)
@@ -32,17 +42,27 @@ taxonomy.results %<>%
     mutate(assignedName=if_else(assigned==TRUE,if_else(epaAssign==TRUE,epaID,epaBestSppID),"NA")) %>%
     mutate(assigned=if_else(isFish==FALSE,FALSE,assigned))
 
-# filter contams and make a label
-contam.table %<>% filter(primer=="tele02") %>% mutate(label=paste(otuCode,bestId,sep="|"))
+
+############## ADD CONTAMINANTS ##############
+############## ADD CONTAMINANTS ##############
+
+# make a label
+contam.table %<>% mutate(label=paste(otuCode,bestId,sep="|"))
 
 # add contam status
 taxonomy.results %<>% mutate(contaminationID=purrr::map_chr(nucleotides,find_matches,referencedb=contam.table), isContaminated=if_else(is.na(contaminationID),FALSE,TRUE)) 
+
+
+############## COMBINE ASVs AND EVENTS ##############
+############## COMBINE ASVs AND EVENTS ##############
 
 # load master asv table
 asvs.all <- suppressMessages(suppressWarnings(read_csv(file=here("temp/taxonomic-assignment/asvs-all.csv"))))
 
 # make id
-asvs.all %<>% mutate(asvCode=paste(primer,lib,asv,sep="-")) %>% select(-asv) %>% rename(asvHash=md5,primerSet=primer,library=lib)
+asvs.all %<>% mutate(asvCode=paste(primer,lib,asv,sep="-")) %>% 
+    select(-asv) %>% 
+    rename(asvHash=md5,primerSet=primer,library=lib)
 
 # get dirs for otu tables
 libs.dirs <- list.dirs(path=here("temp/processing"),full.names=TRUE,recursive=FALSE)
@@ -51,7 +71,9 @@ libs.dirs <- list.dirs(path=here("temp/processing"),full.names=TRUE,recursive=FA
 tables.list <- suppressMessages(suppressWarnings(lapply(here(libs.dirs,"results/asv-table.tsv"),read_tsv)))
 
 # pivot long and merge otu tables
-asv.tables.df <- purrr::map_dfr(tables.list,pivot_longer,cols=-asv,names_to="sampleHash",values_to="nreads") %>% filter(nreads > 0) %>% rename(asvCode=asv)
+asv.tables.df <- purrr::map_dfr(tables.list,pivot_longer,cols=-asv,names_to="sampleHash",values_to="nreads") %>% 
+    filter(nreads > 0) %>% 
+    rename(asvCode=asv)
 
 # load up table of events
 events.list <- suppressMessages(suppressWarnings(lapply(here(libs.dirs,"results/events-hashes.csv"),read_csv)))
@@ -66,10 +88,15 @@ asv.tables.events <- asv.tables.df %>% left_join(events.df,by="sampleHash")
 asvs.by.sample <- asv.tables.events %>% left_join(asvs.all,by=c("asvCode","primerSet","library")) 
 
 # write out the non-homologous reads
-#asvs.by.sample %>% filter(is.na(asvHash)) %>% select(-asvHash) %>% write_csv(here("results/asvs-non-hologous"))
+#asvs.by.sample %>% filter(is.na(asvHash)) %>% select(-asvHash) %>% write_csv(here("results/asvs-non-homologous.csv"))
 
 # add reads to the taxonomy results
-reads.by.asv <- asvs.by.sample %>% filter(!is.na(asvHash)) %>% group_by(asvHash) %>% summarise(nreads=sum(nreads))
+reads.by.asv <- asvs.by.sample %>% 
+    filter(!is.na(asvHash)) %>% 
+    group_by(asvHash) %>% 
+    summarise(nreads=sum(nreads),.groups="drop")
+
+# join
 taxonomy.results %<>% left_join(reads.by.asv,by="asvHash")
 
 # add taxonomy to the asvs.by.sample
@@ -78,37 +105,57 @@ taxonomy.results.reduced <- taxonomy.results %>% select(asvHash,isFish,assigned,
 # remove non-assigned and collapse asvs
 fishes.by.sample <- asvs.by.sample %>% left_join(taxonomy.results.reduced,by="asvHash") %>% 
     filter(isFish==TRUE & assigned==TRUE & isContaminated==FALSE) %>%
-    group_by(primerSet,library,eventID,sampleHash,assignedName) %>% 
+    group_by(primerSet,library,eventID,sampleHash,replicateFilter,assignedName) %>% 
     summarise(nreads=sum(nreads),.groups="drop") %>%
     arrange(primerSet,library,eventID,sampleHash,desc(nreads),assignedName) %>%
     rename(nReads=nreads)
 
+
+############## FILTER AND WRITE OUT ##############
+############## FILTER AND WRITE OUT ##############
+
+# write out fishes after removing blanks
+fishes.by.sample %>% 
+    filter(!grepl("Blank",replicateFilter)) %>% 
+    select(-replicateFilter) %>%
+    write_csv(file=here("results/fishes-by-sample.csv"))
+
+# write out taxonomic identifications
+taxonomy.results %>% select(asvHash,nreads,isFish,assigned,assignedName,
+    sintaxSpeciesID,sintaxBS,kingdom,phylum,class,order,family,genus,
+    blastAssign,blastSpeciesID,blastDbid,blastEvalue,blastLength,blastPident,blastNident,blastScore,blastBitscore,
+    epaAssign,epaIdLWR,epaID,epaBestSppID,epaAllSpp,
+    isContaminated,contaminationID,asvLength,nucleotides) %>%
+    arrange(desc(isFish),desc(assigned),desc(nreads)) %>% 
+    write_csv(file=here("results/taxonomic-assignments.csv"))
+
+
+############## NEGATIVE CONTROLS ##############
+############## NEGATIVE CONTROLS ##############
+
+# isolate blanks with assigned fish reads
+blanks.with.fishes <- fishes.by.sample %>% filter(grepl("Blank",replicateFilter))
+
+# remove samples with reads from all events
+events.df.blanks <- events.df %>% 
+    filter(grepl("Blank",replicateFilter)) %>% 
+    mutate(assignedName=NA,nReads=0) %>% 
+    filter(!sampleHash %in% pull(blanks.with.fishes,sampleHash))
+
+# combine and sort
+blanks.all <- bind_rows(blanks.with.fishes,events.df.blanks) %>% 
+    select(primerSet,library,replicateFilter,eventID,sampleHash,assignedName,nReads) %>%
+    arrange(primerSet,library,replicateFilter,eventID,sampleHash,desc(nReads)) %>%
+    rename(blankType=replicateFilter)
+
 # write out
-fishes.by.sample %>% write_csv(file=here("results/fishes-by-sample.csv"))
+blanks.all %>% write_csv(file=here("results/controls-summary.csv"))
+
+
+############## REPORT ##############
+############## REPORT ##############
 
 # report
-writeLines("\n...\nOutput written to 'results/fishes-by-sample.csv'\n")
-
-## NEED TO do
-# controls
-# taxonomic assignment
-# add non-homol reads to assignments table?
-# suppress messages
-
-
-
-# write
-# taxonomy.results %>% arrange(desc(isFish),assigned,kingdom,phylum,class,order,family,genus) %>% write_csv(file=here("temp/taxonomic-assignment/all.csv"))
-
-
-# check
-#taxonomy.results %>% select(asv,sintaxSpeciesID,assigned,assignedName,contaminationID) %>% arrange(desc(assigned),asv) %>% print(n=Inf)
-
-# take a look
-#taxonomy.results %>% 
-#    select(-kingdom,-phylum,-class,-order,-family,-genus,-propTotal,-dnas,-blastDbid,-blastEvalue,-blastScore,-blastBitscore,-lib) %>%
-#    arrange(desc(assigned),desc(epaAssign),desc(blastAssign)) %>% 
-#    write_csv(file=paste0(proj.path,"/results/sanity-check.csv"))
-
-# write out
-#taxonomy.results %>% write_csv(file=paste0(proj.path,"/results/taxonomic-assignment.csv"))
+writeLines("\n...\nAssigned fish reads by sample written to: 'results/fishes-by-sample.csv'\n")
+writeLines("\n...\nTaxonomic assignments by ASV written to: 'results/taxonomic-assignments.csv'\n")
+writeLines("\n...\nSummary of negative controls written to: 'results/controls-summary.csv'\n")
